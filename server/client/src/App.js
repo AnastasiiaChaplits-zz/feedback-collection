5598f8e60ebd8a920127f2d88d207a4591664850
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2021-01-11 12:14:39 +0200

    Initial react app configuration

---
+++ server/client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href='/auth/google'>Sign in with Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
