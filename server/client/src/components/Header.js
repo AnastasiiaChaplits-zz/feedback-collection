6dde31b09b800dd57bcddee3e03af692e91da164
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2021-01-15 13:53:36 +0200

    Add content to Header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,19 @@ class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return 'Still deciding';
+        return;
       case false:
-        return 'I am loggedout';
+        return (
+          <li>
+            <a href='/auth/google'>Login with Google</a>
+          </li>
+        );
       default:
-        return 'I am loggedin';
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
     }
   }
 
