b7a90bf88db794f9537a2377b31f4bdc0356279f
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2021-01-15 12:41:18 +0200

    Connect action to component

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-import FETCH_USER from './types';
+import { FETCH_USER } from './types';
 
-const fetchUser = () => (dispatch) => {
-  axios
-    .get('/api/current_user')
-    .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+export const fetchUser = () => {
+  return function (dispatch) {
+    axios
+      .get('/api/current_user')
+      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
