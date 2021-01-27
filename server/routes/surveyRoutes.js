caaddd586d7dca4d10faef24fb6343b80adc64ef
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2021-01-27 10:52:01 +0200

    Add last responded date record

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -11,8 +11,8 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for voting!');
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('test');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
@@ -41,7 +41,8 @@ module.exports = (app) => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }
         ).exec();
       })
